347c2cc59c18214f27e7e8e33324751c8a64b707
catch762 <hoh.prichesat@gmail.com>
2026-07-08 21:03:52 +0300

    made stripes

--- py/sv_toolbar.py
+++ py/sv_toolbar.py
@@ -14,3 +14,17 @@ def build_toolbar():
 
 	toolbar = parent.create(td.containerCOMP, name)
 	sv.coordsys_proj.move_node(toolbar, 0)
+	
+	make_stripe(0)
+	make_stripe(1)
+	
+def get_toolbar():
+	return project.op(NODENAME_TOOLBAR)
+	
+def make_stripe(index: int):
+	toolbar, name, existing_stripe = sv.process_projrel_path(f"{NODENAME_TOOLBAR}/svag_stripe_{index}")
+
+	sv.destroy_if_exists(existing_stripe)
+	
+	stripe = toolbar.create(td.containerCOMP, name)
+	coordsys_toolbar.move_node(stripe, 0, 2 * index)
